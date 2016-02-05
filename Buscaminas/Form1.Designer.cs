9a6f04510daaf239d5d4b795b04cae10a2379538
diego <luarca84@gmail.com>
2016-02-05 20:53:55 +0100

    Added Files

---
+++ Buscaminas/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Buscaminas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvMinas = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.txtDificultad = new System.Windows.Forms.TextBox();
+            this.lblDificultad = new System.Windows.Forms.Label();
+            this.txtMinas = new System.Windows.Forms.TextBox();
+            this.lblMinas = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMinas)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.dgvMinas, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(468, 383);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dgvMinas
+            // 
+            this.dgvMinas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMinas.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvMinas.Location = new System.Drawing.Point(3, 83);
+            this.dgvMinas.Name = "dgvMinas";
+            this.dgvMinas.Size = new System.Drawing.Size(462, 297);
+            this.dgvMinas.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnPlay);
+            this.panel1.Controls.Add(this.txtDificultad);
+            this.panel1.Controls.Add(this.lblDificultad);
+            this.panel1.Controls.Add(this.txtMinas);
+            this.panel1.Controls.Add(this.lblMinas);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(462, 74);
+            this.panel1.TabIndex = 1;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(118, 7);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(313, 49);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // txtDificultad
+            // 
+            this.txtDificultad.Location = new System.Drawing.Point(66, 36);
+            this.txtDificultad.Name = "txtDificultad";
+            this.txtDificultad.Size = new System.Drawing.Size(46, 20);
+            this.txtDificultad.TabIndex = 3;
+            this.txtDificultad.Text = "10";
+            // 
+            // lblDificultad
+            // 
+            this.lblDificultad.AutoSize = true;
+            this.lblDificultad.Location = new System.Drawing.Point(9, 39);
+            this.lblDificultad.Name = "lblDificultad";
+            this.lblDificultad.Size = new System.Drawing.Size(51, 13);
+            this.lblDificultad.TabIndex = 2;
+            this.lblDificultad.Text = "Dificultad";
+            // 
+            // txtMinas
+            // 
+            this.txtMinas.Location = new System.Drawing.Point(66, 7);
+            this.txtMinas.Name = "txtMinas";
+            this.txtMinas.Size = new System.Drawing.Size(46, 20);
+            this.txtMinas.TabIndex = 1;
+            this.txtMinas.Text = "10";
+            // 
+            // lblMinas
+            // 
+            this.lblMinas.AutoSize = true;
+            this.lblMinas.Location = new System.Drawing.Point(9, 10);
+            this.lblMinas.Name = "lblMinas";
+            this.lblMinas.Size = new System.Drawing.Size(35, 13);
+            this.lblMinas.TabIndex = 0;
+            this.lblMinas.Text = "Minas";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 383);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscaminas";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMinas)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dgvMinas;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtDificultad;
+        private System.Windows.Forms.Label lblDificultad;
+        private System.Windows.Forms.TextBox txtMinas;
+        private System.Windows.Forms.Label lblMinas;
+    }
+}
+
---
+++ Buscaminas/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Buscaminas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        const int CTE_MINE = 999;
+        int[,] matrix = null;
+        int numMinas = 10;
+        int numDificultad = 10;
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            numMinas = int.Parse(txtMinas.Text);
+            numDificultad = int.Parse(txtDificultad.Text);
+
+            DataTable dt = new DataTable();
+            for (int i = 0; i < numDificultad; i++)
+                dt.Columns.Add();
+            for (int i = 0; i < numDificultad; i++)
+                dt.Rows.Add(dt.NewRow());
+
+            dgvMinas.DataSource = dt;
+            dgvMinas.ColumnHeadersVisible = false;
+            dgvMinas.RowHeadersVisible = false;
+            dgvMinas.AllowUserToAddRows = false;
+            dgvMinas.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dgvMinas.ReadOnly = true;
+            dgvMinas.CellClick -= new DataGridViewCellEventHandler(dgvMinas_CellClick);
+            dgvMinas.CellClick += new DataGridViewCellEventHandler(dgvMinas_CellClick);
+
+            for(int i =0; i< numDificultad;i++)
+                for (int j = 0; j < numDificultad; j++)
+                {
+                    DataGridViewButtonCell b = new DataGridViewButtonCell();
+                    dgvMinas[i, j] = b;
+                }
+
+            matrix = new int[numDificultad,numDificultad];
+            Random r = new Random();
+            for (int n = 0; n < numMinas; n++) 
+            {
+                int i = r.Next(numDificultad);
+                int j = r.Next(numDificultad);
+                if (matrix[i, j] == CTE_MINE)
+                    n--;
+                else
+                    matrix[i, j] = CTE_MINE;
+            }
+
+        }
+
+        void dgvMinas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvMinas.CurrentCell == null)
+                return;
+            dgvMinas[e.ColumnIndex, e.RowIndex] = new DataGridViewTextBoxCell();
+            if (matrix[e.ColumnIndex, e.RowIndex] == CTE_MINE)
+            {
+                dgvMinas[e.ColumnIndex, e.RowIndex].Style.BackColor = Color.Red;
+                dgvMinas.ClearSelection();
+                MessageBox.Show("Game Over");
+            }
+            else
+            {
+                int n = GetNumMinasAlrededorCelda(e.ColumnIndex, e.RowIndex);
+                dgvMinas[e.ColumnIndex, e.RowIndex].Value = n;
+                if (n == 0)
+                {
+                    DespejarCeldasAlrededor(e.ColumnIndex, e.RowIndex);
+                }
+            }
+
+
+            int numButtons = GetNumButtons();
+            if (numButtons == numMinas)
+                MessageBox.Show("You Win");
+
+            dgvMinas.ClearSelection();
+
+        }
+
+        private int GetNumButtons()
+        {
+            int n = 0;
+            for (int i = 0; i < numDificultad; i++)
+                for (int j = 0; j < numDificultad; j++)
+                    if (dgvMinas[i, j].Value.ToString() == string.Empty)
+                    {
+                        n++;
+                    }
+            return n;
+        }
+
+        private void DespejarCeldasAlrededor(int i, int j)
+        {
+            if (i >= 0 && i < numDificultad && j >= 0 && j < numDificultad)
+            {
+                DespejarCeldasAlrededor_Pos(i-1, j-1);
+                DespejarCeldasAlrededor_Pos(i-1, j);
+                DespejarCeldasAlrededor_Pos(i - 1, j+1);
+                DespejarCeldasAlrededor_Pos(i, j - 1);
+                DespejarCeldasAlrededor_Pos(i, j + 1);
+                DespejarCeldasAlrededor_Pos(i + 1, j - 1);
+                DespejarCeldasAlrededor_Pos(i + 1, j);
+                DespejarCeldasAlrededor_Pos(i + 1, j + 1);
+            }
+        }
+
+        private void DespejarCeldasAlrededor_Pos(int a, int b)
+        {
+            int n1 = GetNumMinasAlrededorCelda(a, b);
+
+            if (a >= 0 && a < numDificultad && b >= 0 && b < numDificultad && (dgvMinas[a, b].Value.ToString() == string.Empty))
+            {
+                dgvMinas[a, b].Value = n1;
+                dgvMinas[a, b] = new DataGridViewTextBoxCell();
+
+                if (n1 == 0)
+                {
+                    DespejarCeldasAlrededor(a, b);
+                }
+            }
+            
+            
+        }
+
+        private int GetNumMinasAlrededorCelda(int i, int j)
+        {
+            bool b1 = ExisteMinaEnCelda(i - 1, j - 1);
+            bool b2 = ExisteMinaEnCelda(i - 1, j);
+            bool b3 = ExisteMinaEnCelda(i - 1, j + 1);
+            bool b4 = ExisteMinaEnCelda(i, j - 1);
+            bool b5 = ExisteMinaEnCelda(i, j + 1);
+            bool b6 = ExisteMinaEnCelda(i + 1, j - 1);
+            bool b7 = ExisteMinaEnCelda(i + 1, j );
+            bool b8 = ExisteMinaEnCelda(i + 1, j + 1);
+
+            int n = 0;
+            if (b1) n++;
+            if (b2) n++;
+            if (b3) n++;
+            if (b4) n++;
+            if (b5) n++;
+            if (b6) n++;
+            if (b7) n++;
+            if (b8) n++;
+            return n;
+        }
+
+        private bool ExisteMinaEnCelda(int i, int j)
+        {
+            if (i >= 0 && i < numDificultad && j >= 0 && j < numDificultad)
+                return matrix[i, j] == CTE_MINE;
+            return false;
+        }
+    }
+}
